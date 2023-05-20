1c92ef86d81105cfc7a49f1f5c761ec761831c53
Leul Bayesa <leulbayesa@gmail.com>
2023-05-20 22:45:02 +0000

    updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,13 +3,13 @@
 starts a Flask web application
 """
 
-from flask import Flask, render_template
+from flask import Flask
 app = Flask("__name__")
 
 
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
-    return render_template("10-hbnb_filters.html")
+    return "Hello HBNB!"
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
